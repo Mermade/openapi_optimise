abd278a9d8fe09ac203ba8992a0ab20259273a93
Mike Ralphson <mike.ralphson@gmail.com>
2016-08-25 12:04:44 +0100

    Remove duplicated and unused common parameters

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 A utility to optimise OpenApi (swagger) 2.0 specifications, by
 
 * Converting repeated parameters into common parameters
+* Removing duplicated common parameters
+* Removing unused common parameters
 * Removing redundant consumes/produces
 
 It may be most useful when the OpenApi specification has been converted from another format.
--- index.js
+++ index.js
@@ -10,53 +10,66 @@ function uniq(params,name) {
 	return name+suffix;
 }
 
-function store(cache,param,name,p,action,pa,warn) {
+function store(state,param,name,p,action,pa,common) {
 	var found = false;
-	for (var e in cache) {
-		var entry = cache[e];
+	for (var e in state.cache) {
+		var entry = state.cache[e];
 		if (_.isEqual(entry.definition,param)) {
-			if (entry.name == name) {
-				var location = {};
-				location.path = p;
-				location.action = action;
-				location.index = pa;
-				entry.locations.push(location);
-				found = true;
-				break;
-			}
-			if (warn) {
+			var location = {};
+			location.path = p;
+			location.action = action;
+			location.index = pa;
+			entry.locations.push(location);
+			found = true;
+			if (common) {
 				console.log('Info: common parameters '+entry.name+' and '+name+' are identical');
+				var dupe = {};
+				dupe["from"] = name;
+				dupe["to"] = entry.name;
+				state.rename.push(dupe);
 			}
 		}
 		else if (_.isMatch(entry.definition,param)) {
-			console.log('Info: subset detected '+name);
+			console.log('Info: parameter subset detected');
+			console.log('  '+entry.name+ ' @ '+entry.locations[0].action+' '+entry.locations[0].path);
+			console.log('  '+name+' @ '+action+' '+p);
 		}
 	}
 	if (!found) {
 		var entry = {};
 		entry.definition = param;
-		entry.name = param.name;
+		entry.name = name;
+		entry.common = common;
 		entry.locations = [];
 		var location = {};
 		location.path = p;
 		location.action = action;
 		location.index = pa;
 		entry.locations.push(location);
-		cache.push(entry);
+		state.cache.push(entry);
+	}
+	if (common) {
+		var cp = {};
+		cp.name = name;
+		cp.seen = 0;
+		state.common.push(cp);
 	}
 }
 
 module.exports = {
 
-	optimise : function(src) {
+	optimise : function(src,options) {
 
 		var actions = ['get','head','post','put','delete','patch','options','trace','connect'];
 
-		var cache = [];
+		var state = {};
+		state.cache = [];
+		state.rename = [];
+		state.common = [];
 
 		for (var p in src.parameters) {
 			var param = src.parameters[p];
-			store(cache,param,p,'#','all',-1,true);
+			store(state,param,p,'#/parameters/'+p,'all',-1,true);
 		}
 
 		for (var p in src.paths) {
@@ -67,8 +80,22 @@ module.exports = {
 					for (var pa in action.parameters) {
 						var param = action.parameters[pa];
 
-						if (!param["$ref"]) {
-							store(cache,param,param.name,p,actions[a],pa,false);
+						if (param["$ref"]) {
+							var refName = param["$ref"].replace('#/parameters/','');
+							for (var c in state.common) {
+								var cp = state.common[c];
+								if (cp.name == refName) {
+									duplicate = false;
+									for (var r in state.rename) {
+										var dupe = state.rename[r];
+										if (dupe["from"] == refName) duplicate = true;
+									}
+									if (!duplicate) cp.seen++;
+								}
+							}
+						}
+						else {
+							store(state,param,param.name,p,actions[a],pa,false);
 						}
 					}
 				}
@@ -77,21 +104,63 @@ module.exports = {
 
 		var dest = _.clone(src);
 
-		for (var e in cache) {
-			var entry = cache[e];
+		for (var e in state.cache) {
+			var entry = state.cache[e];
 			if (entry.locations.length>1) {
-				var newName = uniq(dest.parameters,entry.name);
-				if (!dest.parameters) {
-					dest.parameters = {};
+				var newName = entry.name;
+				if (!entry.common) {
+					newName = uniq(dest.parameters,entry.definition.name);
+					if (!dest.parameters) {
+						dest.parameters = {};
+					}
 				}
 				dest.parameters[newName] = entry.definition;
-				console.log('The following parameters can be merged into #/'+newName);
+				console.log('The following parameters can be merged into #/parameters/'+newName);
 				for (var l in entry.locations) {
 					var location = entry.locations[l];
-					console.log('  '+entry.name+' '+location.action+' '+location.path);
-					var newDef = {};
-					newDef["$ref"] = '#/parameters/'+newName;
-					dest.paths[location.path][location.action].parameters[location.index] = newDef;
+					console.log('  '+entry.definition.name+' @ '+location.action+' '+location.path);
+					if (location.action != 'all') {
+						var newDef = {};
+						newDef["$ref"] = '#/parameters/'+newName;
+						dest.paths[location.path][location.action].parameters[location.index] = newDef;
+					}
+				}
+			}
+		}
+
+		if (state.rename.length>0) {
+			console.log('Renaming duplicated common parameters');
+
+			for (var p in dest.paths) {
+				var path = dest.paths[p];
+				for (var a in actions) {
+					var action = path[actions[a]];
+					if (action) {
+						for (var pa in action.parameters) {
+							var param = action.parameters[pa];
+
+							if (param["$ref"]) {
+								var refName = param["$ref"].replace('#/parameters/','');
+								for (var r in state.rename) {
+									var ren = state.rename[r];
+									if (ren["from"] == refName) {
+										param["$ref"] = '#/parameters/'+ren["to"];
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		if (state.common.length>0) {
+			console.log('Checking common parameters are used');
+			for (var c in state.common) {
+				var cp = state.common[c];
+				if (cp.seen <= 0) {
+					console.log('  Deleting '+cp.name);
+					delete dest.parameters[cp.name];
 				}
 			}
 		}
