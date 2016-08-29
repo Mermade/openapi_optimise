7b6af23bcb7f0b042147cd484187534793137e3e
Mike Ralphson <mike.ralphson@gmail.com>
2016-08-29 19:24:47 +0100

    Add deindent option, remove detab.js and add yaml modes

--- openapi_optimise.js
+++ openapi_optimise.js
@@ -1,5 +1,9 @@
 var fs = require('fs');
 var path = require('path');
+
+var yaml = require('js-yaml');
+var j2y = require('jgexml/json2yaml.js');
+
 var opt = require('./index.js');
 var deref = require('./schema_deref.js')
 
@@ -13,14 +17,34 @@ var argv = require('yargs')
 	.describe('verbose','verbosity level, repeat for more logging')
 	.boolean('expand')
 	.alias('e','expand')
-	.describe('expand','expand all local $refs before compression')
+	.describe('expand','expand all local $refs before any model compression')
+	.alias('d','deindent')
+	.boolean('deindent')
+	.describe('deindent','no indentation/linefeeds')
+	.boolean('yaml')
+	.alias('y','yaml')
+	.describe('yaml','read and write specification in yaml format (default JSON)')
+	.boolean('yamlinput')
+	.alias('i','yamlinput')
+	.describe('yamlinput','read specification in yaml format')
+	.boolean('yamloutput')
+	.alias('o','yamloutput')
+	.describe('yamloutput','write specification in yaml format')
 	.demand(1)
 	.help('h')
     .alias('h', 'help')
 	.argv;
 
 var infile = argv._[0];
-var src = require(path.resolve(infile));
+
+var src;
+if ((argv.yaml) || (argv.yamlinput)) {
+	var srcStr = fs.readFileSync(path.resolve(infile),'utf8');
+	src = yaml.safeLoad(srcStr);
+}
+else {
+	src = require(path.resolve(infile));
+}
 
 var dest = opt.defaultOptimisations(src,argv);
 
@@ -33,9 +57,18 @@ if (argv.nondefault) {
 
 var outfile = (argv._.length>1 ? argv._[1] : '');
 
-if (outfile) {
-	fs.writeFileSync(outfile,JSON.stringify(dest,null,'\t'),'utf8');
+var outStr;
+if ((argv.yaml) || (argv.yamloutput)) {
+	outStr = j2y.getYaml(dest);
 }
 else {
-	console.log(JSON.stringify(dest,null,'\t'));
+	var indent = (argv.deindent ? '' : '\t');
+	outStr = JSON.stringify(dest,null,indent);
+}
+
+if (outfile) {
+	fs.writeFileSync(outfile,outStr,'utf8');
+}
+else {
+	console.log(outStr);
 }
