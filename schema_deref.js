2584d46d60b704e3079019ad34d2473fecb2601f
Mike Ralphson <mike.ralphson@gmail.com>
2016-08-29 09:10:30 +0100

    Extra circular.js

--- schema_deref.js
+++ schema_deref.js
@@ -6,127 +6,15 @@
 */
 
 var _ = require('lodash');
-var common = require('./common.js');
 var jptr = require('jgexml/jpath.js');
-
-function dump(defs,title) {
-	console.log(title);
-	for (var d in defs) {
-		var def = defs[d];
-		console.log(def.ref+' '+def.seen+' '+def.children.length);
-		for (var c in def.children) {
-			console.log('  '+def.children[c].ref);
-		}
-	}
-}
-
-function topoSort(src) {
-	// https://en.wikipedia.org/wiki/Topological_sorting
-
-	var defs = [];
-
-	common.recurse(src,{},function(obj,state){
-		if (state.key == '$ref') {
-
-			var entry = {};
-			var found = false;
-			for (var d in defs) {
-				var def = defs[d];
-				if (def.ref == obj) {
-					found = true;
-					def.seen++;
-					entry = def;
-				}
-				if (found) break;
-			}
-			if (!found) {
-				entry.ref = obj;
-				entry.seen = 1;
-				entry.children = [];
-				defs.push(entry);
-			}
-
-			var ref = obj;
-			var restart = jptr.jptr(src,ref);
-
-			//console.log(ref+' '+JSON.stringify(restart));
-
-			var parent = state.parents[state.parents.length-1];
-			var newState = {};
-
-			common.recurse(restart,newState,function(obj,state) {
-				if (state.key == '$ref') {
-					var child = {};
-					child.ref = obj;
-
-					var found = false;
-					for (var c in entry.children) {
-						var compare = entry.children[c];
-						if (compare.ref == obj) {
-							found = true;
-						}
-					}
-					if (!found) {
-						entry.children.push(child);
-					}
-					if (child.ref == entry.ref) {
-						entry.seen++;
-					}
-				}
-			});
-
-		}
-	});
-
-	//dump(defs,' one');
-
-	var changes = 1;
-	while (changes>0) {
-		changes = 0;
-		for (var d in defs) {
-			var def = defs[d];
-			if ((def.seen<=1) || (def.children.length<=0)) {
-
-				for (var dd in defs) {
-					var compare = defs[dd];
-					for (var c in compare.children) {
-						var child = compare.children[c];
-						if (child.ref == def.ref) {
-							compare.children.splice(c,1);
-							changes++;
-						}
-					}
-				}
-
-				defs.splice(d,1);
-				changes++;
-			}
-		}
-
-		//dump(defs,' subsequent');
-	}
-
-	//dump(defs,' final');
-	return defs;
-}
-
-function isCircular(circles,ref) {
-	var result = false;
-	for (var c in circles) {
-		var circle = circles[c];
-		for (var cc in circle.children) {
-			var child = circle.children[cc];
-			if (child.ref == ref) return true;
-		}
-	}
-	return false;
-}
+var common = require('./common.js');
+var circular = require('./circular.js');
 
 module.exports = {
 
 	expand : function(src,options) {
 
-		var circles = topoSort(src);
+		var circles = circular.getCircularRefs(src);
 
 		var lib = _.cloneDeep(src);
 		delete src.parameters;
@@ -152,7 +40,7 @@ module.exports = {
 				if (state.key == '$ref') {
 					var reference = obj;
 
-					if (!isCircular(circles,reference)) {
+					if (!circular.isCircular(circles,reference)) {
 						var result = jptr.jptr(lib,reference);
 						state.parents[state.parents.length-2][state.keys[state.keys.length-2]] = result;
 						changes++;
@@ -170,7 +58,7 @@ module.exports = {
 				if (state.key == '$ref') {
 					var reference = obj;
 
-					if (!isCircular(circles,reference)) {
+					if (!circular.isCircular(circles,reference)) {
 						var result = _.cloneDeep(jptr.jptr(lib,reference));
 						state.parents[state.parents.length-2][state.keys[state.keys.length-2]] = result;
 						changes++;
